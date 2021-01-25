9caee6639928cd3e1254c43c571b4c54fba45acb
Wilson Zheng <wzheng536@gmail.com>
2021-01-24 22:59:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function select(name) {
+  document.getElementById(name).style.background = "red";
+}
 
+function select2(name, oldname, source) {
+  var someimage = document.getElementById(oldname);
+  var theimg = someimage.getElementsByTagName('img')[0];
+  var mysource = myimg.src;
+  
+  someimage.parentNode.removeChild(someimage);
+  
+  var img = document.createElement("img");
+  img.src = mysource;
+  var the_source = document.getElementById(name);
+  the_source.append(img);
+}
