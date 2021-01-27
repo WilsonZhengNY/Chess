2c27fea978f1f9bc394d55325bf009989baa0065
Wilson Zheng <wzheng536@gmail.com>
2021-01-27 13:10:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var color = "white";
+
 function select(name) {
+  color = document.getElementById(name).style.backgroundColor;
   document.getElementById(name).style.background = "red";
 }
 
-function select2(name, oldname, color) {
+function select2(name, oldname) {
   var someimage = document.getElementById(oldname);
   var theimg = someimage.getElementsByTagName('img')[0];
   var mysource = theimg.src;
