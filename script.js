9cc4c5b33cc577da2896541a01fdd55fe78b99c4
Wilson Zheng <wzheng536@gmail.com>
2021-05-10 14:59:05 -0400

    Update script.js
    added alt support when updating images.

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ function select2(name, oldname) {
   var someimage = document.getElementById(oldname);
   var theimg = someimage.getElementsByTagName('img')[0];
   var mysource = theimg.src;
+  var myalt = theimg.alt;
   
   theimg.parentNode.removeChild(theimg);
   someimage.style.background = color;
   
   var img = document.createElement("img");
   img.src = mysource;
+  img.alt = myalt;
   var the_source = document.getElementById(name);
   var the_captured_piece = the_source.getElementsByTagName('img')[0];
   if (typeof the_captured_piece != 'undefined') {
